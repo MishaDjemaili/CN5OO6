3122bbecc5629cf2374250e3251f7748014cfc41
Misha <misha.d2604@outlook.com>
2024-11-05 13:48:09 +0000

    CREATING WEEK 5 BRANCH

---
+++ src/myGreetingApp.js
@@ -0,0 +1,11 @@
+import "./App.css";
+function GreetingsElement() {
+  const greeting = "Hello Function Component";
+  return (
+    <div className="App">
+      <h1>{greeting}</h1>;
+    </div>
+  );
+}
+
+export default GreetingsElement;
